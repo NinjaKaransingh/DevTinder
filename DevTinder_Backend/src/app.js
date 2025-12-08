1f3916419070e51a45f2f6ab257795a6e1cc8ebb
Karansingh B <bkaransinghb@gmail.com>
2025-12-08 20:05:40 +0530

    routing concept

--- DevTinder_Backend/src/app.js
+++ DevTinder_Backend/src/app.js
@@ -2,35 +2,56 @@ const express = require("express");
 
 const app = express(); // instance of express js application
 
-app.get("/", (req, res) => {
-  res.send("Hello from the dashboard!");
-});
-
 //This will only handle GET call to /hello
-app.get("/user", (req, res) => {
+// app.get("/user", (req, res) => {
+//   res.send({
+//     firstName: "Karansingh B",
+//     lastName: "Borde",
+//   });
+// });
+
+//optional routes
+app.get(/^\/ab?c$/, (req, res) => {
+  // /ac or /abc
   res.send({
     firstName: "Karansingh B",
     lastName: "Borde",
   });
 });
 
-app.post("/user", (req, res) => {
-  //saving data to DB
-  res.send("Data successfully saved to the database");
+app.get(/^\/abc\/?$/, (req, res) => {
+  //http://localhost:3000/abc or http://localhost:3000/abc/
+  res.send("Login");
 });
 
-app.delete("/user", (req, res) => {
-  res.send("User Deleted successfully");
+app.get(/^\/ab+ce$/, (req, res) => {
+  //http://localhost:3000/abbbbce -> '+' means we can add multiple b's in the url
+  res.send("abc");
+});
+
+app.get("/ab*cd", (req, res) => {
+  //absdjsndncd - it means it should start with ab and ends with cd in between we can add anything
+  res.send("abcd");
+});
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({
+    firstName: "Karansingh",
+    lastName: "Borde",
+  });
+});
+
+app.get("/user/:userId/:userName", (req, res) => {
+  //Params
+  console.log(req.params);
+  res.send("Params");
 });
 
 //This will match all the HTTP method API calls to /hello
-app.use("/user", (req, res) => {
-  res.send("Hello from the server");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
+// app.use("/user", (req, res) => {
+//   res.send("Hello from the server");
+// });
 
 // app.use((req, res) => {
 //   res.send("Hello from the server!");
