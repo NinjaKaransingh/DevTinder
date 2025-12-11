701a36eebb695c7f3666bfc44a44ad44ac53a656
Karansingh B <bkaransinghb@gmail.com>
2025-12-11 21:26:11 +0530

    signup and feed api creation

---
+++ DevTinder_Backend/src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
